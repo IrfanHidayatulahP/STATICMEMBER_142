9302159443997126284014c66a449508dd0ec7c0
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-05-15 14:33:51 +0700

    menambah kondisi untuk method angka dan cetakData

--- Destruktor/Destruktor.cpp
+++ Destruktor/Destruktor.cpp
@@ -25,6 +25,14 @@ angka::~angka() {				//Destructor
 	cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
 
+void angka::cetakData() {
+	for (int i = 1;i <= panjang; i++) {
+		cout << i << " = "; cin >> arr[i];
+	}
+	cout << endl;
+}
+
+
 
 int main() {
 
