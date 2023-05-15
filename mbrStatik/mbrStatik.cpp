32091cf82b1093622c5300884bf9bdf092cb274c
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-05-15 11:36:17 +0700

    membuat method mahasiswa dengan nim

--- mbrStatik/mbrStatik.cpp
+++ mbrStatik/mbrStatik.cpp
@@ -10,8 +10,16 @@ public:
 
     void setID();
     void printAll();
-
+    mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+    id = ++nim;
+
+}
+
 
 int main()
 {
