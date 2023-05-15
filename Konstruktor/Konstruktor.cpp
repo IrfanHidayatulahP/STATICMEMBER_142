23a2ef06331384561faea248b3e1e300de992528
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-05-15 14:14:23 +0700

    membuat method mahasiswa dan cetak

--- Konstruktor/Konstruktor.cpp
+++ Konstruktor/Konstruktor.cpp
@@ -32,8 +32,10 @@ mahasiswa::mahasiswa(int iNim, string iNama) {
 	nama = iNama;
 }
 
-
+void mahasiswa::cetak() {
+	cout << endl << "Nim	= " << nim << endl;
+	cout << "Nama	= " << nama << endl;
+}
 
 int main() {
-
 }
