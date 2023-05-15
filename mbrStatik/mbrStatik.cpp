aab1852092efebbece94e25d2f64a2cf90fc1f04
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-05-15 11:23:26 +0700

    Add project files.

---
+++ mbrStatik/mbrStatik.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+public:
+    static int nim;
+    int id;
+    string nama;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
