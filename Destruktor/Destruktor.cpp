319b26d7b5387366c1576c8da39c714823aa0366
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-05-15 14:39:31 +0700

    membuat main Program

--- Destruktor/Destruktor.cpp
+++ Destruktor/Destruktor.cpp
@@ -40,5 +40,9 @@ void angka::isiData() {
 }
 
 int main() {
-	angka 
-}
+	angka belajarcpp(3);								//Constructor Dipanggil
+	angka* ptrBelajarcpp = new angka(5);				//Constructor dipanggil
+	delete ptrBelajarcpp;								//Destructor Dipanggil
+
+	return 0;
+}//Destructor Dipanggill
