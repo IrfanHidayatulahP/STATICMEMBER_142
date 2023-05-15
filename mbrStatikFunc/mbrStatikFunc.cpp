a50edb1fbbf4fd61c4938738f5540234e5e66e26
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-05-15 13:45:52 +0700

    membuat method mahasiswa dengan printAll

--- mbrStatikFunc/mbrStatikFunc.cpp
+++ mbrStatikFunc/mbrStatikFunc.cpp
@@ -24,6 +24,13 @@ void mahasiswa::setID() {
     id = ++nim;
 }
 
+void mahasiswa::printAll() {
+    cout << "ID     = " << id << endl;
+    cout << "Nama   = " << nama << endl;
+    cout << endl;
+}
+
+
 
 int main()
 {
